bc7dbc3e722bd56ca508ebfba513cee8aff43b84
viveknexos <146309666+22922511@users.noreply.github.com>
2024-11-23 21:58:56 +0530

    Create Minimize the Heights II.cpp

---
+++ Array/gfg160/Minimize the Heights II.cpp
@@ -0,0 +1,19 @@
+ int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n=arr.size();
+        
+        sort(arr.begin(),arr.end());
+        
+       
+        int ans=arr[n-1]-arr[0];
+        
+        for(int i=1;i<n;i++){
+            
+             if((arr[i]-k)< 0) continue;
+           int  minH=min(arr[0]+k,arr[i]-k);
+           int  mxH=max(arr[n-1]-k,arr[i-1]+k);
+           ans=min(ans,mxH-minH);
+            
+        }
+        return ans;
+    }
