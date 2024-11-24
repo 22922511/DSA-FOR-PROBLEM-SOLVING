28144695c4749f221a76d6ffe1454299aefb2d08
viveknexos <146309666+22922511@users.noreply.github.com>
2024-11-24 12:04:18 +0530

    Create Bellman ford.cpp

---
+++ Graph/Bellman ford.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+int INF=1e17;
+vector<vector<int>>gp;
+vector<int>dis;
+
+vector<int> bell(int start){
+     dis[start]=0;
+     for(int i=0;i<n;i++){
+         
+         for(vector<int>dist:gp){
+             int u = dist[0];
+             int v = dist[1];
+             int d = dist[2];
+             
+             if(dis[u]!=INF && dis[v]>dis[u]+d){
+                 if(i==n-1) return {-1};
+                 dis[v]=dis[u]+d;
+             }
+         }
+     }
+     return dis;
+}
+int main()
+{
+   cin>>n>>m;
+   dis.assign(n,INF);
+   
+   for(int i=0;i<m;i++){
+       int a,b,c;
+       cin>>a>>b>>c;
+       gp.push_back({a,b,c});
+   }
+bell(0);
+for(int id:dis) cout<<id<<" ";
+
+ 
+    return 0;
+}
