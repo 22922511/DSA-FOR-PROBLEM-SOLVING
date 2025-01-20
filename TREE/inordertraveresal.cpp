a54e6e2492a5446545a7d91e6855e8ec6ab60f88
viveknexos <146309666+22922511@users.noreply.github.com>
2025-01-20 16:15:42 +0530

    Create inordertraveresal.cpp

---
+++ TREE/inordertraveresal.cpp
@@ -0,0 +1,61 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int val;
+    Node *left, *right;
+    Node(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+vector<int>ans;
+void inoder(Node*root){
+    if(root==NULL) return;
+
+    inoder(root->left);
+    ans.push_back(root->val);
+    inoder(root->right);
+}
+vector<int> getInorderTraversal(Node* root) {
+  ans.clear();
+    inoder(root);
+    return ans; 
+}
+
+
+Node* getBinaryTree(vector<int> &num, int*ind) {
+    if(num[*ind] == -1)
+        return NULL;
+    Node* node = new Node(num[*ind]);
+    (*ind)++;
+    node->left = getBinaryTree(num,ind);
+    (*ind)++;
+    node->right = getBinaryTree(num,ind);
+    return node;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        int ind = 0;
+        Node* tree = getBinaryTree(arr,&ind);
+        vector<int> ans = getInorderTraversal(tree);
+        for(auto v:ans){
+            cout<<v<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
